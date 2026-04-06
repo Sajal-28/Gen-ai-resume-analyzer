b36ae9f5131be2f516d09bb99dfb6714827f5ca6
Sajal Dubey <dubeysajal007@gmail.com>
2026-04-06 10:52:29 +0530

    feat: implement authentication hook and update login page with state management

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,38 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../auth.form.scss"
-import { useNavigate,Link } from 'react-router'
+import { useNavigate, Link } from 'react-router'
+import { useAuth } from "../hooks/useAuth"
 
 const Login = () => {
 
+    const { loading, handleLogin } = useAuth();
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
     const handleSubmit = (e) => {
         e.preventDefault();
     }
 
 
-  return (
-    <main>
-        <div className="form-container">
-            <h1>Login</h1>
+    return (
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
 
-            <form onSubmit={handleSubmit}>
-                <div className="input-group">
-                    <label htmlFor="Email">Email</label>
-                    <input type="text" placeholder='Enter Email Address' id='email' name='email' />
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="Email">Email</label>
+                        <input
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            type="text" placeholder='Enter Email Address' id='email' name='email' />
+                    </div>
 
-                <div className="input-group">
-                    <label htmlFor="Password">Password</label>
-                    <input type="text" placeholder='Enter Password' id='password' name='password' />
-                </div>
+                    <div className="input-group">
+                        <label htmlFor="Password">Password</label>
+                        <input
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            type="text" placeholder='Enter Password' id='password' name='password' />
+                    </div>
 
-                <button className='button primary-button'>Login</button>
-            </form>
+                    <button className='button primary-button'>Login</button>
+                </form>
 
-            <p>Don't have an account ? <Link to={"/register"}>Register</Link></p>
-        </div>
-    </main>
-  )
+                <p>Don't have an account ? <Link to={"/register"}>Register</Link></p>
+            </div>
+        </main>
+    )
 }
 
 export default Login
