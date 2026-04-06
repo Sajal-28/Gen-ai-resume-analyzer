c6963fc14e0a2beb7ebaff57bc35187769e540f1
Sajal Dubey <dubeysajal007@gmail.com>
2026-04-06 10:24:15 +0530

    feat: add authentication context and API service for user management

---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+// const baseUrl = import.meta.env.VITE_API_URL
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true
+})
+
+export async function register({ username, email, password }) {
+    try {
+        const response = await api.post(`/api/auth/register`, {
+            username, email, password
+        })
+
+        return response.data
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export async function login({ email, password }) {
+    try {
+        const response = await api.post(`/api/auth/login`, {
+            email, password
+        })
+
+        return response.data
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function logout() {
+    try {
+        const response = await api.get(`/api/auth/logout`)
+
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function getMe() {
+    try {
+        const response = await api.get(`/api/auth/me`)
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
