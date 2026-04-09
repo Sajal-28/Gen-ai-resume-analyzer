59c806597e3ab39d665fdbd1299095547b554b29
Sajal Dubey <dubeysajal007@gmail.com>
2026-04-09 14:53:37 +0530

    fix: downgrade puppeteer to version 19.11.1 and remove executable path from launch options

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -59,7 +59,6 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
 async function generatePdfFromHtml(htmlContent) {
     const browser = await puppeteer.launch({
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
     const page = await browser.newPage();
